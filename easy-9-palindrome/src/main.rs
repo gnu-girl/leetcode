4534ec22ca2705173124f902efb18839e847ffed
Anita Slater <heyheyheyppl@gmail.com>
2023-11-13 00:12:08 -0500

    found better solution

--- easy-9-palindrome/src/main.rs
+++ easy-9-palindrome/src/main.rs
@@ -1,5 +1,10 @@
+use std::env;
+
 mod solution;
 
 fn main() {
-    println!("{}", solution::is_palindrome(1001));
+    let args: Vec<String> = env::args().collect();
+    let input: i32 = args[1].parse::<i32>().unwrap();
+
+    println!("input:   {:?}\nsolution: {:?}",args[1], solution::is_palindrome_no_math(input));
 }
--- easy-9-palindrome/src/solution.rs
+++ easy-9-palindrome/src/solution.rs
@@ -1,6 +1,20 @@
+pub fn is_palindrome_no_math(x: i32) -> bool {
+    // Do this the string-y way
+    let digits = x.to_string().chars().collect::<String>();
+    let digits_backwards = digits.chars().rev().collect::<String>();
+    
+    for (i,_el) in digits.chars().enumerate() {
+        if !digits.as_bytes()[i].eq(&digits_backwards.as_bytes()[i]) {
+            return false;
+        }
+    }
+    true
+}
+
 pub fn is_palindrome(mut x: i32) -> bool {
-    let mut digits: Vec<i32> = Vec::new();
-    x = 1001;
+    // Do this the math-y way (break up into digits)
+    let mut digits = Vec::new();
+
     if x >= 0 {
         // negative numbers are never a palindrome
         while x >= 1 {
